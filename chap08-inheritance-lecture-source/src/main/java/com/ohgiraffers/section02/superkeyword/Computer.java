0ae887f754a8a7a0e23f888b54a5d78bc8a39274
bobbobdirara <dhfldmlwkrdmsdusaht@gmail.com>
2024-02-08 18:03:56 +0900

    java

---
+++ chap08-inheritance-lecture-source/src/main/java/com/ohgiraffers/section02/superkeyword/Computer.java
@@ -0,0 +1,81 @@
+package com.ohgiraffers.section02.superkeyword;
+
+import java.util.Date;
+
+public class Computer extends Product {
+
+    private String cpu;
+    private int hdd;
+    private int ram;
+    private String operationSystem;
+
+    public Computer() {
+        System.out.println("Computer 클래스의 기본 생성자 호출함");
+    }
+
+    public Computer(String cpu, int hdd, int ram, String operationSystem) {
+        this.cpu = cpu;
+        this.hdd = hdd;
+        this.ram = ram;
+        this.operationSystem = operationSystem;
+
+        System.out.println("Computer 클래스 모든 필드를 초기화하는 생성자 호출함");
+    }
+
+    public Computer(String code, String brand, String name, int price, Date manufacturingDate, String cpu, int hdd, int ram, String operationSystem) {
+        super(code, brand, name, price, manufacturingDate);
+        this.cpu = cpu;
+        this.hdd = hdd;
+        this.ram = ram;
+        this.operationSystem = operationSystem;
+
+        System.out.println("Computer 클래스의 부모 필드도 초기화하는 생성자 호출함");
+    }
+
+    //setter <-- 부모거는 작성하지 않고 쓸수 있어서 지금 것만 작성
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public void setHdd(int hdd) {
+        this.hdd = hdd;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public void setOperationSystem(String operationSystem) {
+        this.operationSystem = operationSystem;
+    }
+
+    //getter
+    public String getCpu() {
+        return cpu;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public String getOperationSystem() {
+        return operationSystem;
+    }
+
+    @Override
+    public String getInformation() {
+//        return super.getCode() <-- 하나씩 불러올때는 이렇게
+//                                   또한, super와 this는 함께(동시에) 쓸 수 없음
+        return super.getInformation()   //this.getInformation으로 작성하면 오류.. this 로 작성하면 자신의 메소드를 계속 호출함 (재귀호출) -> StackOverflow
+                + " Computer ["
+                + "cpu=" + this.cpu
+                + ", hdd=" + this.hdd
+                + ", ram=" + this.ram
+                + ", operationSystem=" + this.operationSystem
+                + "]";
+    }
+}
